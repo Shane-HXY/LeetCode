a14ecd0780c5acab6475d6ae7ad414719aa36b7b
黄翔宇 <zzuiehxy@outlook.com>
2016-12-08 15:28:40 +0800

    16.12.8 15:28 DSaDI

--- src/hard/DSaDI.java
+++ src/hard/DSaDI.java
@@ -20,9 +20,7 @@ Credits:
 Special thanks to @yunhong for adding this problem and creating most of the test cases.
 */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Definition for an interval.
@@ -33,70 +31,59 @@ import java.util.List;
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
+
+class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
 public class DSaDI {    //  Data Stream as Disjoint Intervals
 
-    private static List<Integer> streams = new ArrayList<>();
-//    static int[] streams = new int[65536];
-    private static int res = 0;
-    private static List<Interval> orgInterval;
-
-    class Interval {
-        int start;
-        int end;
-        Interval() { start = 0; end = 0; }
-        Interval(int s, int e) { start = s; end = e; }
-    }
+    private TreeSet<Interval> intervals;
     /** Initialize your data structure here. */
     public DSaDI() {
+        intervals = new TreeSet<>(new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.start - o2.start;
+            }
+        });
     }
 
     public void addNum(int val) {
-        streams.set(res++, val);
+        Interval interval = new Interval(val, val);
+        Interval floor = intervals.floor(interval);
+        if (floor != null) {
+            if (floor.end >= val) return;
+            else if (floor.end + 1 == val) {
+                interval.start = floor.start;
+                intervals.remove(floor);
+            }
+        }
+        Interval higher = intervals.higher(interval);
+        if (higher != null && (higher.start - 1 == val)) {
+            interval.end = higher.end;
+            intervals.remove(higher);
+        }
+        intervals.add(interval);
     }
 
     public List<Interval> getIntervals() {
-        int cur = streams.get(res);
-        boolean[] line = new boolean[res];
-        Arrays.fill(line, false);
-        int linePos = 0;
-        for (Interval interval : orgInterval) {
-            if (cur >= interval.start && cur <= interval.end);
-            else if (1 == (interval.start - cur) || 1 == (cur - interval.end)) {
-                if (cur < interval.start) {
-                    interval.start = cur;
-                } else {
-                    interval.end = cur;
-                }
-                line[linePos] = true;
-            } else {
-                orgInterval.add(new Interval(cur, cur));
-            }
-            linePos++;
-        }
-        Interval itv = null;
-        for (int k = 0; k < line.length; k++) {
-            if (line[k]) {
-                if (itv != null) {
-                    Interval nxtItv = orgInterval.get(k);
-                    if (nxtItv.start == itv.end) {
-                        
-                    }
-                }
-                itv = orgInterval.get(k);
-            }
-        }
+        return (Arrays.asList(intervals.toArray(new Interval[0])));
     }
 
     public static void main(String[] args) {
-        int val = 1;
-        DSaDI obj = new DSaDI();
-        int i = 0;
-        while (streams.get(res) >= 0) {
-
+        DSaDI di = new DSaDI();
+        int[] intList = {1, 3, 7, 2, 6};
+        for (int i = 0; i < intList.length; i++) {
+            di.addNum(intList[i]);
+            for (Interval itv:
+                    di.getIntervals()) {
+                System.out.print("[" + itv.start + ", " + itv.end + "] ");
+            }
+            System.out.print("\n");
         }
-        obj.addNum(val);
-        List<Interval> param_2 = obj.getIntervals();
-
     }
 }
 
